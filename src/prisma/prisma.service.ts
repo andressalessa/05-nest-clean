5c9d63f422a343110f9edab1c250d3076f5f60c2
Andressa Lessa <s.andressalessa@gmail.com>
2026-08-12 20:46:41 -0300

    core: fixing prisma connection

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from 'prisma/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    // Fix caused by prisma 7.2.0 update
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const databaseURL = new URL(process.env.DATABASE_URL!)
+    const schema = databaseURL.searchParams.get('schema')
+
+    const adapter = new PrismaPg(
+      { connectionString: databaseURL.toString() },
+      { schema: schema || 'public' },
+    )
+
+    super({
+      adapter,
+      log: ['warn', 'error'],
+    })
+  }
+
+  async onModuleInit() {
+    await this.$connect()
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect()
+  }
+}
