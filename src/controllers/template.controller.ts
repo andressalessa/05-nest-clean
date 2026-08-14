e70b27186b59ec890d8dc7094128f38316c4b7ab
Andressa Lessa <s.andressalessa@gmail.com>
2026-08-14 09:29:30 -0300

    core: changing some configurations

--- src/controllers/template.controller.ts
+++ src/controllers/template.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, UseGuards} from "@nestjs/common";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { PrismaService } from "src/prisma/prisma.service";
+import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
+import { PrismaService } from "@/prisma/prisma.service";
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
